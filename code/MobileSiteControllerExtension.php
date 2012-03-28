2c4a067e69a0c8e2b29ffff6f9ccdfcb8e342b5e
Morven Lewis-Everley <morven@i-lateral.com>
2012-03-28 22:57:32 +0100

    Fix issue of silent fallover created by trying to save a non session object

--- code/MobileSiteControllerExtension.php
+++ code/MobileSiteControllerExtension.php
@@ -56,9 +56,6 @@ class MobileSiteControllerExtension extends Extension {
 		$fullSiteCookie = Session::get('fullSite');
 
 		if(is_numeric($fullSiteCookie)) {
-                    $fullSiteCookie->set_timeout(self::$cookie_expire_time);
-                    $fullSiteCookie->save();
-                    
 			// Full site requested
 			if($fullSiteCookie) {
 				if($this->onMobileDomain() && $config->MobileSiteType == 'RedirectToDomain') {
