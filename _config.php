ffed3c941ce9571da1c9c7d0d3434027193d7ef4
Morven Lewis-Everley <morven@i-lateral.com>
2013-11-18 14:14:53 +0000

    Update extension so that we extend controller (this allows security to also make use of mobile)

--- _config.php
+++ _config.php
@@ -2,5 +2,5 @@
 define('MOBILE_DIR', 'mobile');
 
 Object::add_extension('SiteConfig', 'MobileSiteConfigExtension');
-Object::add_extension('ContentController', 'MobileSiteControllerExtension');
+Object::add_extension('Controller', 'MobileSiteControllerExtension');
 Object::add_extension('SiteTree', 'MobileSiteTreeExtension');
